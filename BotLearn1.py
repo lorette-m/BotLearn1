96c3c1949197d7dd8eabb99a4b66b9c3cbe5bba8
lorette-m <lory_works@outlook.com>
2024-02-05 19:23:54 +0300

    ver1

--- BotLearn1.py
+++ BotLearn1.py
@@ -1,10 +1,28 @@
 import requests
+import time
 
-api_url = 'http://api.open-notify.org/iss-now.json'
 
-response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
+TEXT = 'Ура! Классный апдейт!'
+MAX_COUNTER = 100
 
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    print(response.text)
-else:
-    print(response.status_code)    # При другом коде ответа выводим этот код
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
