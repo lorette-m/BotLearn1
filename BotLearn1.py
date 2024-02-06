a3fe2ff5bbdf1a86d079e0aada0bb975088a3438
lorette-m <lory_works@outlook.com>
2024-02-06 07:33:11 +0300

    echo1

--- BotLearn1.py
+++ BotLearn1.py
@@ -1,32 +1,25 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message 
 
-
-API_URL = 'https://api.telegram.org/bot'
-API_FOX_URL = 'https://randomfox.ca/floof/'
 BOT_TOKEN = '6358456657:AAGeGX8wUfEcgE5kOXK9rWBVr-3OnjK_0To'
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
-offset = -2
-counter = 0
-fox_response: requests.Response
-fox_link: str
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-
-while counter < 100:
-    print('attempt =', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            fox_response = requests.get(API_FOX_URL)
-            if fox_response.status_code == 200:
-                fox_link = fox_response.json()['image']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={fox_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-
-    time.sleep(1)
-    counter += 1
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+        )
+   
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+    
+if __name__ == '__main__':
+    dp.run_polling(bot)
