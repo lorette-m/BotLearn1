31427976440d9e278900aed4b3b765b4c74d53a2
lorette-m <lory_works@outlook.com>
2024-02-06 07:44:51 +0300

    echo3photo

--- BotLearn1.py
+++ BotLearn1.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message 
 
@@ -14,12 +14,16 @@ async def process_help_command(message: Message):
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
         )
+    
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
 async def send_echo(message: Message):
     await message.reply(text=message.text)
     
 dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
 dp.message.register(send_echo)
     
 if __name__ == '__main__':
