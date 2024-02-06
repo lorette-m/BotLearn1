e020f9280b494802de6017dd436bb23201bdd8b9
lorette-m <lory_works@outlook.com>
2024-02-06 07:35:38 +0300

    echo2

--- BotLearn1.py
+++ BotLearn1.py
@@ -6,20 +6,21 @@ BOT_TOKEN = '6358456657:AAGeGX8wUfEcgE5kOXK9rWBVr-3OnjK_0To'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command(commands=['start']))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
         )
-   
-@dp.message()
+
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+    
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_echo)
     
 if __name__ == '__main__':
     dp.run_polling(bot)
